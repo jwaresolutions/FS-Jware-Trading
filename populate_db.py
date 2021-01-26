38344db1cc3f04ad93264305e84670cfc0341c62
jwaresolutions <iGUR2QCei2smYoWMh6*9>
2021-01-26 06:01:43 -0800

    inital commit

---
+++ populate_db.py
@@ -0,0 +1,37 @@
+import sqlite3
+import config as config
+import alpaca_trade_api as tradeapi
+
+API_KEY = config.get_keys(request='key_id', dryrun=False)
+SECRET_KEY = config.get_keys(request='secret_key', dryrun=False)
+API_URL = config.get_keys(request='endpoint', dryrun=False)
+
+connection = sqlite3.connect(config.db_path)
+connection.row_factory = sqlite3.Row
+
+cursor = connection.cursor()
+
+api = tradeapi.REST(
+    API_KEY,
+    SECRET_KEY,
+    base_url=API_URL
+)  # or use ENV Vars shown below
+assets = api.list_assets()
+
+cursor.execute("""
+    SELECT symbol, name FROM stock
+""")
+rows = cursor.fetchall()
+symbols = [row["symbol"] for row in rows]
+
+for asset in assets:
+    try:
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            cursor.execute("INSERT INTO stock (symbol, name, exchange) VALUES (?, ?, ?)",
+                           (asset.symbol, asset.name, asset.exchange))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
+
+connection.commit()
