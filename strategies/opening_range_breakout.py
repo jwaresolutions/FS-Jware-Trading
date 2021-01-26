2916a81252b0843c002e14f31fae88ac507ab399
jwaresolutions <iGUR2QCei2smYoWMh6*9>
2021-01-26 15:13:56 -0800

    tenetivly working

--- strategies/opening_range_breakout.py
+++ strategies/opening_range_breakout.py
@@ -3,11 +3,14 @@ import config
 import alpaca_trade_api as tradeapi
 from datetime import date
 
+current_date = date.today().isoformat()
+API_KEY = config.get_keys(request='key_id', dryrun=True)
+SECRET_KEY = config.get_keys(request='secret_key', dryrun=True)
+API_URL = config.get_keys(request='endpoint', dryrun=True)
+api = tradeapi.REST(API_KEY, SECRET_KEY, base_url=API_URL)
 
-API_KEY = config.get_keys(request='key_id', dryrun=False)
-SECRET_KEY = config.get_keys(request='secret_key', dryrun=False)
-API_URL = config.get_keys(request='endpoint', dryrun=False)
-
+orders = api.list_orders(status='all', after=f"{current_date}T13:30:00Z")
+existing_order_symbols = [order.symbol for order in orders]
 connection = sqlite3.connect(config.db_path)
 connection.row_factory = sqlite3.Row
 
@@ -30,8 +33,7 @@ stocks = cursor.fetchall()
 symbols = [stock['symbol'] for stock in stocks]
 print(symbols)
 
-api = tradeapi.REST(API_KEY, SECRET_KEY, base_url=API_URL)
-current_date = date.today().isoformat()
+
 start_minute_bar = f"{current_date} 09:30:00-05:00"
 end_minute_bar = f"{current_date} 09:45:00-05:00"
 
@@ -47,9 +49,26 @@ for symbol in symbols:
     after_opening_range_bars = minute_bars.loc[after_opening_range_mask]
     after_opening_range_breakout = after_opening_range_bars[after_opening_range_bars['close'] > opening_range_high]
 
-    if not after_opening_range_breakout.empty:
+    if not after_opening_range_breakout.empty and symbol not in existing_order_symbols:
         limit_price = after_opening_range_breakout.iloc[0]['close']
-        print(f"placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]['close']}")
+        print(
+            f"placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]['close']}")
 
+        api.submit_order(
+            symbol=symbol,
+            side='buy',
+            type='limit',
+            qty='1',
+            time_in_force='day',
+            order_class='bracket',
+            limit_price=limit_price,
+            take_profit=dict(
+                limit_price=limit_price + opening_range,
+            ),
+            stop_loss=dict(
+                stop_price=limit_price - opening_range,
+            )
+        )
 
-
+    else:
+        print(f"Order for {symbol} alredy exists, current list of orders {existing_order_symbols}")
