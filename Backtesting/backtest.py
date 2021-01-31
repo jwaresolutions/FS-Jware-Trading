6422c9acca83be4616a0905a84ecacc6819c0658
jwaresolutions <iGUR2QCei2smYoWMh6*9>
2021-01-31 15:47:37 -0800

    wth

--- Backtesting/backtest.py
+++ Backtesting/backtest.py
@@ -16,6 +16,7 @@ class OpeningRangeBreakout(backtrader.Strategy):
         self.opening_range = 0
         self.bought_today = False
         self.order = None
+        self.direction = 'None'
 
     def log(self, txt, dt=None):
         if dt is None:
@@ -50,6 +51,7 @@ class OpeningRangeBreakout(backtrader.Strategy):
             self.opening_range_low = self.data.low[0]
             self.opening_range_high = self.data.high[0]
             self.bought_today = False
+            self.direction = 'None'
 
         opening_range_start_time = time(9, 30, 0)
         dt = datetime.combine(date.today(), opening_range_start_time) + timedelta(minutes=self.p.num_opening_bars)
@@ -64,14 +66,34 @@ class OpeningRangeBreakout(backtrader.Strategy):
             if self.order:
                 return
 
-            if self.position and (self.data.close[0] > (self.opening_range_high + self.opening_range)):
+            if self.position and (self.data.close[0] > (
+                    self.opening_range_high + self.opening_range) and self.direction == 'Breakout'):
                 self.close()
 
-            if self.data.close[0] > self.opening_range_high and not self.position and not self.bought_today:
+            if self.position and (self.data.close[0] < (
+                    self.opening_range_low - self.opening_range) and self.direction == 'Breakdown'):
+                self.close()
+
+            if self.data.close[
+                0] > self.opening_range_high and not self.position and not self.bought_today and self.data.close[0] > \
+                    self.data.sma_20[0] and self.data.close[0] > self.data.sma_50[0]:
                 self.order = self.buy()
                 self.bought_today = True
+                self.direction = "Breakout"
 
-            if self.position and (self.data.close[0] < (self.opening_range_high - self.opening_range)):
+            if self.data.close[
+                0] < self.opening_range_low and not self.position and not self.bought_today and self.data.close[0] < \
+                    self.data.sma_20[0] and self.data.close[0] < self.data.sma_50[0]:
+                self.order = self.sell()
+                self.bought_today = True
+                self.direction = "Breakdown"
+
+            if self.position and (self.data.close[0] < (
+                    self.opening_range_high - self.opening_range) and self.direction == 'Breakout'):
+                self.order = self.close()
+
+            if self.position and (self.data.close[0] > (
+                    self.opening_range_low + self.opening_range) and self.direction == 'Breakdown'):
                 self.order = self.close()
 
             if self.position and current_bar_datetime.time() >= time(15, 45, 0):
@@ -83,10 +105,13 @@ class OpeningRangeBreakout(backtrader.Strategy):
                  (self.params.num_opening_bars, self.broker.getvalue()))
 
         if self.broker.getvalue() > 130000:
-            self.log("*** BIG WINNER ***")
+            self.log(f"*** {self.current_symbol} BIG WINNER ***")
 
-        if self.broker.getvalue() < 70000:
-            self.log("*** MAJOR LOSER ***")
+        elif self.broker.getvalue() < 70000:
+            self.log(f"*** {self.current_symbol} MAJOR LOSER ***")
+
+        else:
+            self.log(f"*** {self.current_symbol} INEFFECTIVE ***")
 
 
 if __name__ == '__main__':
@@ -97,6 +122,9 @@ if __name__ == '__main__':
         SELECT DISTINCT(stock_id) as stock_id FROM stock_price_minute
     """)
     stocks = cursor.fetchall()
+
+    stocks = [stocks[0]]
+
     for stock in stocks:
         print(f"== Testing {stock['stock_id']} ==")
 
@@ -105,7 +133,7 @@ if __name__ == '__main__':
         cerebro.addsizer(backtrader.sizers.PercentSizer, percents=95)
 
         dataframe = pandas.read_sql("""
-            select datetime, open, high, low, close, volume
+            select datetime, open, high, low, close, volume, sma_20, sma_50, rsi_14
             from stock_price_minute
             where stock_id = :stock_id
             and strftime('%H:%M:%S', datetime) >= '09:30:00' 
--- DB/populate_minute_data.py
+++ DB/populate_minute_data.py
@@ -1,6 +1,6 @@
 import alpaca_connection
-import sqlite3, pandas
-from datetime import datetime, timedelta, date
+import sqlite3, numpy, tulipy
+from datetime import timedelta, date
 from strategies.Helpers import api_call
 
 alpaca_connect = alpaca_connection.Alpaca_Connect()
@@ -9,13 +9,16 @@ connection.row_factory = sqlite3.Row
 api = api_call()
 cursor = connection.cursor()
 
+today = date.today()
+end_date_range = today - timedelta(days=today.weekday())
+
 cursor.execute("""
     select symbol, stock_strategy.stock_id
     from stock
     join stock_strategy on stock_strategy.stock_id = stock.id
     join stock_price on stock.id = stock_price.stock_id
     where date = ? and strategy_id = 1
-""", ('2021-01-29',))
+""", (end_date_range.isoformat(),))
 
 stocks = cursor.fetchall()
 
@@ -24,22 +27,26 @@ stock_id = [stock['stock_id'] for stock in stocks]
 stocks_dict = dict(zip(symbols, stock_id))
 
 for symbol in stocks_dict:
-    today = date.today()
-    end_date_range = today - timedelta(days=today.weekday())
+    recent_closes = []
     start_date = end_date_range - timedelta(weeks=52)
 
     while start_date < end_date_range:
-        end_date = start_date + timedelta(days=4)
-        print(symbol)
-        minutes = api.polygon.historic_agg_v2(symbol, 1, 'minute', _from=start_date, to=end_date).df
+        minutes = api.polygon.historic_agg_v2('AMC', 1, 'minute', _from=start_date, to=start_date).df
         minutes = minutes.resample('1min').ffill()
-        print(minutes)
         for index, row in minutes.iterrows():
+            recent_closes.append(row['close'])
+            if len(recent_closes) >= 50:
+                sma_20 = tulipy.sma(numpy.array(recent_closes), period=20)[-1]
+                sma_50 = tulipy.sma(numpy.array(recent_closes), period=50)[-1]
+                rsi_14 = tulipy.rsi(numpy.array(recent_closes), period=14)[-1]
+            else:
+                sma_20, sma_50, rsi_14 = None, None, None
             cursor.execute("""
-                INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
+                INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume, sma_20, sma_50, rsi_14)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """, (stocks_dict[symbol], index.tz_localize(None).isoformat(), row['open'], row['high'], row['low'], row['close'],
-                  row['volume']))
+                  row['volume'], sma_20, sma_50, rsi_14))
 
         connection.commit()
-        start_date = start_date + timedelta(days=7)
+        start_date = start_date + timedelta(days=1)
+    start_date = start_date + timedelta(days=2)
