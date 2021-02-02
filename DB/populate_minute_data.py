028c9ed62a92428041bd872620e6394809294d76
jwaresolutions <iGUR2QCei2smYoWMh6*9>
2021-02-02 09:04:01 -0800

    testing

--- DB/populate_minute_data.py
+++ DB/populate_minute_data.py
@@ -2,6 +2,7 @@ import alpaca_connection
 import sqlite3, numpy, tulipy
 from datetime import timedelta, date
 from strategies.Helpers import api_call
+import time
 
 alpaca_connect = alpaca_connection.Alpaca_Connect()
 connection = sqlite3.connect(alpaca_connect.db_path)
@@ -9,6 +10,7 @@ connection.row_factory = sqlite3.Row
 api = api_call()
 cursor = connection.cursor()
 
+sleep_count = 0
 today = date.today()
 end_date_range = today - timedelta(days=today.weekday())
 
@@ -31,8 +33,12 @@ for symbol in stocks_dict:
     start_date = end_date_range - timedelta(weeks=52)
 
     while start_date < end_date_range:
+        if sleep_count >= 199:
+            time.sleep(70)
+            sleep_count = 0
         end_date = start_date + timedelta(days=4)
         minutes = api.polygon.historic_agg_v2(symbol, 1, 'minute', _from=start_date, to=end_date).df
+        sleep_count = sleep_count + 1
         minutes = minutes.resample('1min').ffill()
         for index, row in minutes.iterrows():
             recent_closes.append(row['close'])
@@ -42,11 +48,11 @@ for symbol in stocks_dict:
                 rsi_14 = tulipy.rsi(numpy.array(recent_closes), period=14)[-1]
             else:
                 sma_20, sma_50, rsi_14 = None, None, None
-            cursor.execute("""
-                INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume, sma_20, sma_50, rsi_14)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """, (stocks_dict[symbol], index.tz_localize(None).isoformat(), row['open'], row['high'], row['low'],
-                  row['close'], row['volume'], sma_20, sma_50, rsi_14))
-        print(f"== Fetching minute bars for {symbol} {start_date} - {end_date} ==")
+            # cursor.execute("""
+            #     INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume, sma_20, sma_50, rsi_14)
+            #     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            # """, (stocks_dict[symbol], index.tz_localize(None).isoformat(), row['open'], row['high'], row['low'],
+            #       row['close'], row['volume'], sma_20, sma_50, rsi_14))
+        print(f"== COUNTER = {sleep_count} == Fetching minute bars for {symbol} {start_date} - {end_date} ==")
         start_date = start_date + timedelta(days=7)
-connection.commit()
+# connection.commit()
